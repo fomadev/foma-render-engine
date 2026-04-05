89f928a1d70f2d941f8dde8ae0d912c7a0b456b3
Fordi Malanda <fordimalanda7@gmail.com>
2026-04-05 18:19:27 +0200

    fix: resolve ESM build conflicts and cleanup juice type definitions

---
+++ tsup.config.cjs
@@ -0,0 +1,10 @@
+// tsup.config.cjs
+module.exports = {
+  entry: ['src/index.ts'],
+  format: ['cjs', 'esm'],
+  dts: true,
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  minify: true
+};
