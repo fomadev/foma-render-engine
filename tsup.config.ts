a29ca692838afe30aafd738975b30779257cd655
Fordi Malanda <fordimalanda7@gmail.com>
2026-04-05 18:25:01 +0200

    chore: migrate to ESM module type and stabilize typescript configuration

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,11 @@
-// tsup.config.cjs
-module.exports = {
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
   dts: true,
   splitting: false,
   sourcemap: true,
   clean: true,
-  minify: true
-};
+  minify: true,
+});
